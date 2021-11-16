2c2b2121543e42a29afbdb45899d6eab5509617a
ItayHasidi <74491361+ItayHasidi@users.noreply.github.com>
2021-11-16 18:11:56 +0200

    Update DestroyBullet.cs

--- Assets/Scripts/DestroyBullet.cs
+++ Assets/Scripts/DestroyBullet.cs
@@ -5,20 +5,6 @@ using UnityEngine.UI;
 
 public class DestroyBullet : MonoBehaviour
 {
-    // private Rigidbody rb;
-    // private TouchDetector td;
-    // void Start() {
-    //     rb = GetComponent<Rigidbody>();
-    //     td = GetComponent<TouchDetector>();
-    // }
-    // /*
-    //  * Note that updates related to the physics engine should be done in FixedUpdate and not in Update!
-    //  */
-    // private void FixedUpdate() {
-    //     if (td.IsTouching()) {  // allow to walk and jump 
-    //         Destroy(this.gameObject);    
-    //     }
-    // }
     [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
     [SerializeField] string triggeringTag;
 
